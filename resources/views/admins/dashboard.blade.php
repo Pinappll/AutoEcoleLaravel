d4696ca6d5a77911f4f3109a92b518b547476d8d
Joshua Grandville <joshua77130@live.fr>
2024-07-17 18:14:43 +0200

    redirection dashboard

--- resources/views/admins/dashboard.blade.php
+++ resources/views/admins/dashboard.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
+        
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
@@ -31,6 +32,16 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                    
                     <a href="{{ route('students.index') }}" class="text-blue-500 hover:text-blue-700">{{ __('Liste des élèves') }}</a>
+
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                   
+                    <a href="{{ route('lessons.index') }}" class="text-blue-500 hover:text-blue-700">{{ __('Planning') }}</a>
 
                 </div>
             </div>
