d5b15de6a18319ed0a2c27d64c7546ae75027ea7
Joshua Grandville <joshua77130@live.fr>
2024-07-18 16:32:37 +0200

    redirection si tentative d'acces a la page no_role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'role.redirect' => \App\Http\Middleware\RedirectIfNoRole::class,
+            'no_role' => \App\Http\Middleware\NoRoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
